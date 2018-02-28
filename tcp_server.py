7f3f73a6c50fb9b723a2912f7118d36691a9f24b
nikogiyou <nikogiyou@gmail.com>
2018-02-28 11:26:34 +0900

    by_masterwindows

---
+++ tcp_server.py
@@ -0,0 +1,36 @@
+# -*- config: utf-8 -*-
+
+import socket
+import threading
+
+bind_ip = "10.146.0.3"
+bind_port = 8441
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip,bind_port))
+server.listen(5)
+
+print "[*] Listening on %s:%d" % (bind_ip,bind_port)
+
+#クライアントからの接続を処理するスレッド
+def handle_client(client_socket):
+    
+    #クライアントが送信してきたデータを表示
+    request = client_socket.recv(1024)
+    
+    print "[*] Receved: %s" % request
+    
+    #パケットの返送
+    client_socket.send("ACK!")
+    client_socket.close()
+    
+while True:
+    
+    client,addr = server.accept()
+    
+    print "[*] Accepted connection from: %s:%d" % (addr[0],addr[1])
+    
+    #受信データを処理するスレッドの起動
+    client_handler = threading.Thread(target=handle_client,args=(client,))
+    client_handler.start()
+    
