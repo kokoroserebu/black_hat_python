d6f6dfd6270769ede10d1079c24db5553f2d544d
nikogiyou <nikogiyou@gmail.com>
2018-02-28 11:53:21 +0900

    By_master_windows

--- tcp_client.py
+++ tcp_client.py
@@ -2,8 +2,8 @@
 
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "221.251.190.130"
+target_port = 8441
 
 
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -12,9 +12,9 @@ client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect((target_host,target_port))
 
 
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send("happy birthday!")
 
 
 response = client.recv(4096)
 
-print response
+print(response)
